51b61d3c189c74aaf188d79a7c54c30a83b45c0c
a41220 <ducthang642002@gmail.com>
2023-07-21 16:17:26 +0700

    ud

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,6 +1,7 @@
 
 const { createCustomerService, createManyCustomerService, getAllCustomersService, updateACustomersService, deleteACustomersService, deleteManyCustomersService } = require('../services/customerService')
 const { uploadSingleFile } = require("../services/fileService");
+const Joi = require('joi');
 
 //co the viet kieu nay, do la object nen phai co key:value, key la ten, value la function
 module.exports = {
@@ -19,32 +20,58 @@ module.exports = {
         let { name, address, phone, email, description } = req.body //image thi` phai dung thu vien req.files
         // console.log("req.body: ", name, description);
 
-        let imageUrl = ""
-        if (!req.files || Object.keys(req.files).length === 0) {
-            //do nothing
-        } else { //co file
-            let image = await uploadSingleFile(req.files.image);
-            // console.log("result: ", image);
-            imageUrl = image.path
-        }
-
-        //truyen data tu req qua service
-        let customerData = {
-            name,
-            address,
-            phone,
-            email,
-            description,
-            image: imageUrl
-        }
-
-        let customer = await createCustomerService(customerData)
-        // console.log(">>>customer: ", customer);
-
-        return res.status(200).json({
-            EC: 0,
-            data: customer
+        //validate with joi (sau nay nen dinh nghia cai nay o file rieng biet, o day se rất rối)
+        const schema = Joi.object({
+            name: Joi.string()
+                .alphanum()
+                .min(3)
+                .max(30)
+                .required(),
+            address: Joi.string(),
+            phone: Joi.string().pattern(new RegExp('^[0-9]{9,11}$')), //.pattern() chi co tac dung vs string()
+            //required number 0-9, min 9, max 11 number
+            email: Joi.string().email(),
+            description: Joi.string(),
+            image: Joi.string().allow('') //allow null or have to be string value for this field
         })
+        //abortEarly false tuc la se thong bao het tat ca cac loi, true thi chi tbao 1 loi
+        // const { error } = schema.validate(req.body, { abortEarly: false }); 
+        const { error } = schema.validate(req.body); //neu ko co loi no chi return object rong, tuc la ko co error
+        // console.log(">>>result joi: ", result);
+        if (error) { //neu co error ta return message 
+            return res.status(200).json({
+                ms: error
+            })
+
+        } else {
+            let imageUrl = ""
+            if (!req.files || Object.keys(req.files).length === 0) {
+                //do nothing
+            } else { //co file
+                let image = await uploadSingleFile(req.files.image);
+                // console.log("result: ", image);
+                imageUrl = image.path
+            }
+
+            //truyen data tu req qua service
+            let customerData = {
+                name,
+                address,
+                phone,
+                email,
+                description,
+                image: imageUrl
+            }
+
+            let customer = await createCustomerService(customerData)
+            // console.log(">>>customer: ", customer);
+
+            return res.status(200).json({
+                EC: 0,
+                data: customer
+            })
+        }
+
     },
 
     postCreateManyCustomer: async (req, res) => {
